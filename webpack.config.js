abb6d9e383595a484bbf608610ccbb2fd6db3354
Yash Chopra <yashchoprasmail@gmail.com>
2025-09-12 20:23:12 +0530

    Update webpack to fix the build

--- webpack.config.js
+++ webpack.config.js
@@ -22,58 +22,55 @@ module.exports = {
 	},
 
 	module: {
-        rules: [
-            // Keep WordPress default rules but add our custom ones
-            ...defaultConfig.module.rules.filter(rule => {
-                // Filter out default CSS/SCSS rules to avoid conflicts
-                return !(rule.test && rule.test.toString().includes('css|scss'));
-            }),
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
+        ...defaultConfig.module,
+		rules: [
+            //...defaultConfig.module.rules,
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: [
-                            '@babel/preset-env',
-                            ['@babel/preset-react', { runtime: 'automatic' }]
-                        ]
+                      presets: ['@babel/preset-react']
                     }
-                },
-            },
-            {
-                test: /\.(css|scss)$/,
-                exclude: /node_modules/,
-                use: [
-                    MiniCssExtractPlugin.loader, // Don't use style-loader with MiniCssExtractPlugin
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            esModule: false,
-                        }
-                    },
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.svg$/,
-                type: 'asset/inline',
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: '../images/[name][ext][query]',
-                },
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: '../fonts/[name][ext][query]',
-                },
-            },
-        ],
+                }
+			},
+			{
+				test: /\.(css|scss)$/,
+				exclude: /node_modules/,
+				use: [
+					'style-loader',
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							esModule: false,
+						},
+					},
+					{
+						loader: 'css-loader',
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.svg/,
+				type: 'asset/inline',
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				type: 'asset/resource',
+				generator: {
+					filename: '../images/[name][ext][query]',
+				},
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				type: 'asset/resource',
+				generator: {
+					filename: '../fonts/[name][ext][query]',
+				},
+			},
+		],
 	},
 
 	plugins: [
